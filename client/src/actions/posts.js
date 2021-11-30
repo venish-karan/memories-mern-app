c4491ae69bcfa57cdff68faefc5729ce47ec32a4
Venish P <sec19ec070@sairamtap.edu.in>
2021-11-30 19:13:38 +0530

    third commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -18,3 +18,12 @@ export const getPosts = () => async (dispatch) => {
 
    // dispatch(action);
 }
+
+export const createPost = (post) => async (dispatch) => {  try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data });
+     } catch (error) {
+        console.log(error);
+     }
+}
